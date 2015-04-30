0b1469accf2132233acfd61f97bd11492bf29917
Emre Can Yılmaz <emrecan@ecylmz.com>
2015-04-30 15:52:04 +0300

    initial commit

---
+++ lib/globalsms.rb
@@ -0,0 +1,42 @@
+require "globalsms/version"
+require 'httpclient'
+require 'json/ext'
+
+module GlobalSMS
+  class GlobalSMS
+    def initialize(api_base_url, api_key, api_secret)
+      @api_base_url = api_base_url
+      @api_key = api_key
+      @api_secret = api_secret
+    end
+    
+    def single_send(argv)
+      argv = {
+        time: "now",
+        turkish_character: "1"
+      }.merge(argv)
+
+      [:originator, :numbers, :text].each { |arg| puts "ERROR: #{arg} expecting" and return unless argv[arg] }
+
+      body = "data=#{argv.to_json.to_s}"
+      uri = "#{api_base_url}/sms/send/single?key=#{api_key}&secret=#{api_secret}"
+
+      c = HTTPClient.new
+      c.post(uri, body)
+    end
+
+    def bulk_send(argv)
+      argv = {
+        time: "now",
+        turkish_character: "1"
+      }.merge(argv)
+
+      [:originator, :numbers, :text].each { |arg| puts "ERROR: #{arg} expecting" and return unless argv[arg] }
+
+      body = "data=#{argv.to_json.to_s}"
+      uri = "#{api_base_url}/sms/send/single?key=#{api_key}&secret=#{api_secret}"
+
+      c = HTTPClient.new
+      c.post(uri, body)
+  end
+end
---
+++ lib/globalsms/version.rb
@@ -0,0 +1,3 @@
+module Globalsms
+  VERSION = "0.1.0"
+end
