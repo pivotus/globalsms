4153e6c7c662423551788fb59f908aa95169be69
Salih Özdemir <salih.ozdemir@bil.omu.edu.tr>
2015-05-04 17:04:22 +0300

    raporları çalışır kıl

--- lib/globalsms/report.rb
+++ lib/globalsms/report.rb
@@ -16,7 +16,13 @@ module GlobalSMS
       return JSON.parse(c.get(uri).body)
     end
 
-    def bulk_report
+    def bulk_report(argv)
+      res = {}
+      argv.each do |message_id|
+        body = JSON.parse(HTTPClient.new.get("#{@api_base_url}/sms/report/#{message_id}?key=#{@api_key}&secret=#{@api_secret}").body)
+        res[message_id] = body
+      end
+      return res
     end
   end
 end
